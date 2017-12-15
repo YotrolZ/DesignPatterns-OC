29e83c01677e554645661d3b4739b95dd337f66e
YotrolZ <674317983@qq.com>
2017-12-15 16:51:03 +0800

    适配工程并且添加01_简单工厂模式client端使用的代码

--- 设计模式_OC/01_简单工厂模式/ViewController.m
+++ 设计模式_OC/01_简单工厂模式/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "YCarFactory.h"
 
 @interface ViewController ()
 
@@ -16,14 +17,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+ 
+    id<YCar> car = [YCarFactory creatCarWithCarType:YCarTypeBenz];
+    [car run];
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
--- 设计模式_OC/01_简单工厂模式/案例/角色2_抽象产品/YCar.h
+++ 设计模式_OC/01_简单工厂模式/案例/角色2_抽象产品/YCar.h
@@ -9,7 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @protocol YCar <NSObject>
-
 - (void)run;
-
 @end
